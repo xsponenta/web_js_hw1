52e5360036564e869d211c133d846e22a9c26194
IgorIvanyshyn <116706459+xsponenta@users.noreply.github.com>
2024-03-03 04:07:49 +0200

    add file
    first commit

---
+++ task1.js
@@ -0,0 +1,27 @@
+function round(n){
+    return n.toFixed(n);
+}
+
+function reverseWords(str){
+    const words = str.split(' ');
+    const reversedWords = words.reverse();
+    return reversedWords.join(' ');
+}
+
+function onlyEven(arr){
+    return arr.filter(n => n%2 === 0);
+}
+
+function isPalindrome(n) {
+    const nString = String(n);
+    const splittedString = nString.split('');
+    const reversedString = splittedString.slice().reverse();
+    const reversedStr = reversedString.join('');
+    return nString === reversedStr;
+}
+
+function fizzBuzz(n) {
+    return Array.from({ length: n }, (_, i) =>
+        (i + 1) % 3 === 0 ? ((i + 1) % 5 === 0 ? 'FizzBuzz' : 'Fizz') :
+            ((i + 1) % 5 === 0 ? 'Buzz' : String(i + 1)));
+}
